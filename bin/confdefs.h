bd2487d21dd967e27c35ac1844d54b15c4c3ed51
climjark <climjark@protonmail.com>
2018-05-24 17:01:14 -0400

    Im pretty sure this is a very screwed up autotools setup, but this is what i got so far. I will have to add plugins to a bin somehow, maybe it be a lib linked to muktcore at the end of the compilation? Let me know what you think.
    Signed-off-by: climjark <climjark@protonmail.com>

---
+++ bin/confdefs.h
@@ -0,0 +1,20 @@
+/* confdefs.h */
+#define PACKAGE_NAME "Mukt"
+#define PACKAGE_TARNAME "mukt"
+#define PACKAGE_VERSION "0.1"
+#define PACKAGE_STRING "Mukt 0.1"
+#define PACKAGE_BUGREPORT "climjark@protonmail.com"
+#define PACKAGE_URL ""
+#define PACKAGE "mukt"
+#define VERSION "0.1"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_UNISTD_H 1
--- core/core.c
+++ core/core.c
@@ -1,1 +1,9 @@
 #include "../include/core/mukt-core.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+    printf("Testing Automake Build...\n");
+
+  return 0;
+}
